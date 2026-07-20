9aa5ce249f89a4099e8f682cf511da678af6da30
SergeyKurnosov <kurnosovsergej123@mail.ru>
2026-07-20 22:29:16 +1000

    Cookie page

--- 06-CokiesAndSession/cookie.php
+++ 06-CokiesAndSession/cookie.php
@@ -6,7 +6,32 @@
     } else {
         setcookie('return', '1', time() + 300);
     }
-echo '<h1>У этой страницы обязательна кодировка \'Unicode(UTF-8without signature)-Codepage65001)\'</h1>';
-    echo $visitor ? 'Welcome back' : 'Hello';
+
     
     ?>
+
+
+
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Cookies </title>
+</head>
+<body>
+        <h1>Cookies </h1> 
+    <h1>У этой страницы обязательна кодировка <br>Unicode(UTF-8without signature)-Codepage65001)</h1>
+
+    <h2>
+
+     <?= $visitor ? 'Welcome back' : 'Hello';?>
+
+    </h2>
+      
+
+
+
+
+</body>
+</html>
