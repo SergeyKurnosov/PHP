c1cac4cb7c35fa1821c442871d1e82ec473e7bf2
SergeyKurnosov <kurnosovsergej123@mail.ru>
2026-07-23 13:43:14 +1000

    hw done

--- 04-Quitz/inc/quitz.php
+++ 04-Quitz/inc/quitz.php
@@ -1,12 +1,12 @@
 <?php
 
 require_once __DIR__ . "/header.php";
-    try{
-  //  echo "Привет {$_POST['first_name']} {$_POST['last_name']}, приятного прохождения ";
+try {
+    //  echo "Привет {$_POST['first_name']} {$_POST['last_name']}, приятного прохождения ";
     echo "Привет {$first_name} {$last_name}, приятного прохождения ";
-    }catch(Exception $e){
+} catch (Exception $e) {
 
-    }
+}
 echo '<pre>';
 print_r(ROOT . "\n");
 echo $_SERVER['DOCUMENT_ROOT'];
@@ -14,29 +14,21 @@ echo '</pre>';
 
 ?>
 
-<?php
-$visitor = false;
-if (isset($_COOKIE['theme'])) {
-    $visitor = true;
-} else {
-    setcookie('theme', $theme , time() + 300);
-}
-?>
 
-<!-- From Uiverse.io by m1her --> 
+<!-- From Uiverse.io by m1her -->
 <div class="radio-input">
-  <label class="label">
-    <input value="value-1" name="value-radio" id="value-1" type="radio" />
-    <span class="text">Light</span>
-  </label>
-  <label class="label">
-    <input value="value-1" name="value-radio" id="value-1" type="radio" />
-    <span class="text">Dark</span>
-  </label>
-  <label class="label">
-    <input value="value-1" name="value-radio" id="value-1" type="radio" />
-    <span class="text">Green</span>
-  </label>
+    <label class="label">
+        <input value="light_theme" name="value-radio" id="value-1" type="radio" />
+        <span class="text">Light</span>
+    </label>
+    <label class="label">
+        <input value="dark_theme" name="value-radio" id="value-1" type="radio" />
+        <span class="text">Dark</span>
+    </label>
+    <label class="label">
+        <input value="green_theme" name="value-radio" id="value-1" type="radio" />
+        <span class="text">Green</span>
+    </label>
 </div>
 
 
@@ -65,35 +57,40 @@ if (isset($_COOKIE['theme'])) {
 
 <script>
 
-    const radioButtons = document.querySelectorAll('input[name="value-radio"]');
-    radioButtons.forEach(radio => {
-    radio.addEventListener('change', function () {
-        if (this.checked) {
-           let request = new XMLHttpRequest();
-            request.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    alert(this.responseText);
+    <?php if (isset($_COOKIE['theme'])) { ?>
+                            document.body.className = '<?php echo "{$_COOKIE['theme']}" ?>'
+    <?php } ?>
+
+
+        const radioButtons = document.querySelectorAll('input[name="value-radio"]');
+        radioButtons.forEach(radio => {
+        radio.addEventListener('change', function () {
+            if (this.checked) {
+               let request = new XMLHttpRequest();
+                request.onreadystatechange = function() {
                 }
-            }
-            switch(this.value){
-                case "dark_theme":
-                    request.open("GET", "dark.php", true);
-                    request.send();
-                break;
-                case "light_theme":
-                    request.open("GET", "light.php", true);
-                    request.send();
-                break;
-                case "green_theme":
-                    request.open("GET", "green.php", true);
-                    request.send();
-                break;
-            }
+                switch(this.value){
+                    case "dark_theme":
+                        request.open("GET", "dark.php", true);
+                        request.send();
+                location.reload();
+                    break;
+                    case "light_theme":
+                        request.open("GET", "light.php", true);
+                        request.send();
+                location.reload();
+                    break;
+                    case "green_theme":
+                        request.open("GET", "green.php", true);
+                        request.send();
+                location.reload();
+                    break;
+                }
 
-        }
+            }
+        });
+
     });
-
-});
 
 
 </script>
