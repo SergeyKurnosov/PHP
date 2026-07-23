9d425da263c9c678ff62f28d81e05c1a8e23559e
SergeyKurnosov <kurnosovsergej123@mail.ru>
2026-07-23 13:23:08 +1000

    classwork done

--- 07-OOP/inc/main.php
+++ 07-OOP/inc/main.php
@@ -10,13 +10,13 @@ require_once __DIR__ . '/inheritance.php';
 //$A->y = 8;
 //echo $A;
 
-$human = new Human("Vercetty", "Tommy", 30);
+$human = new Human("Vercetty","Tommy",30);
 echo $human . '<br>';
 
 //$t_student = new Student($human, "Theft", "Vice", 90, 99);
 
 $student = new Student("Pinkamn", "Jessie", 20, "Chemistry", "WW_220", 90, 95);
-echo $student;
+echo $student . '<br>';
 
 $graduate = new Graduate("Shreder", "Hank", 40, "Criminalistic", "OBN", 50, 70, "How to catch Hisenberg");
 echo $graduate . '<br>';
